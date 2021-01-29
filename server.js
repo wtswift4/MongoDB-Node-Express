9ac1b969a6148969e3722620871603a35485fe70
Tom Swift <wtswift4@yahoo.com>
2021-01-29 16:20:49 -0600

    Intro to Express Part 1

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
